2fdff38316914c84df379033069636abb7784b2f
carsten11 <carstenpetersen@Sveinns-MacBook-Air.local>
2013-09-26 00:45:12 +0000

    Ground and Floor added. Sprites needs to be modified

--- iOS App Dev/Game.m
+++ iOS App Dev/Game.m
@@ -16,10 +16,30 @@
     self = [super init];
     if (self)
     {
+        
+        [self setupGraphicsLandscape];
         // Your initilization code goes here
         [self scheduleUpdate];
     }
     return self;
+}
+
+- (void) setupGraphicsLandscape
+{
+    
+    CCLayerColor *sky = [CCLayerColor layerWithColor:ccc4(0, 0, 150, 255)];
+    [self addChild:sky];
+    
+    //Ground and Ceiling pictures still needs to be modified....
+    CCSprite *Ground = [CCSprite spriteWithFile:@"Ground.png"];
+    Ground.anchorPoint = ccp(0,0);
+    [self addChild:Ground];
+    
+    CCSprite *Ceiling = [CCSprite spriteWithFile:@"Ceiling.png"];
+    Ceiling.anchorPoint = ccp(0,-5.5);
+    [self addChild:Ceiling];
+    
+    
 }
 
 - (void)update:(ccTime)delta
